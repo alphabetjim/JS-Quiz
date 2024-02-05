24345f6ef661294dba6ee9cb4dfc1a5eb838397c
James Bridgwater Court <james.bcourt@outlook.com>
2024-02-05 16:05:42 +0000

    Add functionality to choose quiz topic

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,16 +1,28 @@
 // const questions = [["What was the title of the George Orwell's dystopian novel featuring the phrase 'doublethink'?", "1984"], ["What is the name given to the Friday before Easter?", "Good Friday"]];
 var questionNo = 1;
 var score = 0;
-const API_URL = "https://opentdb.com/api.php?amount=10&category=13&difficulty=medium&type=multiple";
+const API_URL = "https://opentdb.com/api.php?amount=10&category=20&difficulty=medium&type=multiple";
+const API_URL_ROOT = "https://opentdb.com/api.php?amount=10&category=";
+const API_URL_TAIL = "&difficulty=medium&type=multiple";
 let questionData = [];
+const categories=[['Musicals', 13], ['Mythology',20], ['Geography',22]];
 
 async function getQuestions() {
-    const response = await fetch(API_URL);
+    let categorySelected=0;
+    let API_Code = 20;
+    for (let i =1; i<4; i++){
+        if (document.getElementById(`quizCat${i}`).checked){
+            API_Code = categories[i-1][1];
+        }
+    }
+    document.getElementsByClassName('quiz-select-div')[0].style.display = 'none';
+    const response = await fetch(`${API_URL_ROOT}${API_Code}${API_URL_TAIL}`);
     console.log(response);
     var data = await response.json();
     if (response.ok) {
         questionData = data.results;
         setup();
+        document.getElementsByClassName('quiz-div')[0].style.display = 'block';
     } else {
         console.error('API error');
         throw new Error(data.error);
